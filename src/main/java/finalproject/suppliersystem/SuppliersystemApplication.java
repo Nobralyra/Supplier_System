0cf7b76b389e51a4497c2f72d9b433346eaa4d9b
Nobralyra <signees96@gmail.com>
2021-04-23 15:03:59 +0200

    First commit

---
+++ src/main/java/finalproject/suppliersystem/SuppliersystemApplication.java
@@ -0,0 +1,15 @@
+package finalproject.suppliersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuppliersystemApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SuppliersystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/finalproject/suppliersystem/SuppliersystemApplicationTests.java
@@ -0,0 +1,15 @@
+package finalproject.suppliersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuppliersystemApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
