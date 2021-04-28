a82aa51c45cf007cf6971d225bd2bce2cfb7f203
Nobralyra <signees96@gmail.com>
2021-04-28 15:21:08 +0200

    Added the first draft of Supplier

--- src/main/java/finalproject/suppliersystem/supplier/domain/Supplier.java
+++ src/main/java/finalproject/suppliersystem/supplier/domain/Supplier.java
@@ -1,4 +1,34 @@
 package finalproject.suppliersystem.supplier.domain;
 
-public class Supplier {
+public class Supplier
+{
+    private Long supplierId;
+
+    private String supplierName;
+
+    private int supplierNumber;
+
+    private String callingCode;
+
+    private String businessPhoneNumber;
+
+    private String businessEmail;
+
+    private String webpage;
+
+    private String streetName;
+
+    private String buildingNumber;
+
+    private int postalCode;
+
+    private String postalDistrict;
+
+    private String country;
+
+    private String nameContactPerson;
+
+    private String emailContactPerson;
+
+    private String phoneNumberContactPerson;
 }
