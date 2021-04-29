b930ccfad0f02ff230a883491cc18ca7fbf19db7
Päivi Eversbusch <paiv0018@stud.kea.dk>
2021-04-29 08:17:42 +0200

    repository

---
+++ src/main/java/finalproject/suppliersystem/supplier/repository/ISupplierRepository.java
@@ -0,0 +1,15 @@
+package finalproject.suppliersystem.supplier.repository;
+
+import finalproject.suppliersystem.supplier.domain.Supplier;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Repository creates the connection to our database.
+ * If we use login with Spring Security, we can use method
+ * findByUsename to load the user from database by username:
+ * Optional<Supplier> findByUsername(String username);
+ */
+
+@Repository
+public interface ISupplierRepository extends JpaRepository<Supplier, Long> { }
