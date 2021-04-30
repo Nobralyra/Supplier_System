ccb115764206c3acd5f7d10ee96f4ce8b051eb12
Nobralyra <signees96@gmail.com>
2021-04-30 11:48:38 +0200

    Added extra domain classes to the getmapping, so it can be reached from the supplier.html

--- src/main/java/finalproject/suppliersystem/supplier/controller/SupplierController.java
+++ src/main/java/finalproject/suppliersystem/supplier/controller/SupplierController.java
@@ -1,6 +1,10 @@
 package finalproject.suppliersystem.supplier.controller;
 
+import finalproject.suppliersystem.supplier.domain.Address;
+import finalproject.suppliersystem.supplier.domain.ContactInformation;
+import finalproject.suppliersystem.supplier.domain.ContactPerson;
 import finalproject.suppliersystem.supplier.domain.Supplier;
+import finalproject.suppliersystem.supplier.service.ProductCategoryService;
 import finalproject.suppliersystem.supplier.service.SupplierService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,16 +19,32 @@ import java.util.List;
 public class SupplierController {
 
     private final SupplierService supplierService;
+    private final ProductCategoryService productCategoryService;
 
-    public SupplierController(SupplierService supplierService) {
+    public SupplierController(SupplierService supplierService, ProductCategoryService productCategoryService) {
         this.supplierService = supplierService;
+        this.productCategoryService = productCategoryService;
     }
 
-    @GetMapping("/supplier/register_supplier")
-    public String showRegisterSupplier(Supplier supplier, Model model){
+    /**
+     * Ville det være en ide at ændre til /registration/supplier i stedet for?
+     * Det samme med den mappe HTML'en ligger i?
+     * @param supplier
+     * @param model
+     * @return
+     */
+    @GetMapping("/registration/supplier")
+    public String showRegisterSupplier(Supplier supplier, ContactInformation contactInformation, Address address, ContactPerson contactPerson, Model model){
         model.addAttribute("supplier", supplier);
-        return "/supplier/register_supplier";
+        model.addAttribute("contactInformation", contactInformation);
+        model.addAttribute("address", address);
+        model.addAttribute("contactPerson", contactPerson);
+
+        // her kunne en product Category findAll() godt give mening, men skal den have sin egen service og repository?
+        model.addAttribute("productCategory", productCategoryService.findAll());
+        return "/registration/supplier";
     }
+
 
     /*
         @PostMapping("/bruger/opret_bruger")
@@ -55,10 +75,10 @@ public class SupplierController {
     }
      */
 
-    @PostMapping("/supplier/register_supplier")
+    @PostMapping("/registration/supplier")
     public String registerSupplier(@Valid Supplier supplier, BindingResult bindingResult, Model model){
 
-        if(supplierService.hasErrors(bindingResult, model)) return "/supplier/register_supplier";
+        if(supplierService.hasErrors(bindingResult, model)) return "/registration/supplier";
         List<Supplier> alSuppliers = supplierService.findAll();
 
         return "tester";
