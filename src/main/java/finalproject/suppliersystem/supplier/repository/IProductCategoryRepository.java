5ec069fcb16f5374da9d284ddd9b627ab15f769c
Nobralyra <signees96@gmail.com>
2021-04-30 11:47:52 +0200

    Added ProductCategory service and repository

--- src/main/java/finalproject/suppliersystem/supplier/repository/IProductCategoryRepository.java
+++ src/main/java/finalproject/suppliersystem/supplier/repository/IProductCategoryRepository.java
@@ -1,2 +1,8 @@
-package finalproject.suppliersystem.supplier.repository;public class IProductCategoryRepository {
+package finalproject.suppliersystem.supplier.repository;
+
+import finalproject.suppliersystem.supplier.domain.ProductCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IProductCategoryRepository extends JpaRepository<ProductCategory, Long>
+{
 }
---
+++ src/main/java/finalproject/suppliersystem/supplier/service/ProductCategoryService.java
@@ -0,0 +1,58 @@
+package finalproject.suppliersystem.supplier.service;
+
+import finalproject.suppliersystem.core.IService;
+import finalproject.suppliersystem.supplier.domain.ProductCategory;
+import finalproject.suppliersystem.supplier.repository.IProductCategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ProductCategoryService implements IService<ProductCategory>
+{
+    private final IProductCategoryRepository iProductCategoryRepository;
+
+    /**
+     * Constructor injection
+     * We do not have to specify @Autowired, as long as the class only have one constructor and the class itself
+     * is annotated with a Spring bean, because Spring automatic make the dependency to be injected via the constructor.
+     * It is used here just for readability
+     *
+     * To understand how constructor injection works:
+     * https://stackoverflow.com/questions/40620000/spring-autowire-on-properties-vs-constructor
+     * https://reflectoring.io/constructor-injection/
+     *
+     * @param iProductCategoryRepository - interface of provided methods
+     */
+    @Autowired
+    public ProductCategoryService(IProductCategoryRepository iProductCategoryRepository)
+    {
+        this.iProductCategoryRepository = iProductCategoryRepository;
+    }
+
+    @Override
+    public void save(ProductCategory element)
+    {
+
+    }
+
+    @Override
+    public ProductCategory findById(Long id)
+    {
+        return null;
+    }
+
+    @Override
+    public List<ProductCategory> findAll()
+    {
+        return new ArrayList<>(iProductCategoryRepository.findAll());
+    }
+
+    @Override
+    public void deleteByID(Long id)
+    {
+
+    }
+}
