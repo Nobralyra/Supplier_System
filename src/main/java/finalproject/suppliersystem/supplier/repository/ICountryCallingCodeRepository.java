1b78e57833e761a9714bcae021d36c0942e3b984
Nobralyra <signees96@gmail.com>
2021-05-03 16:05:49 +0200

    Added CountryCallingCodeRepository

--- src/main/java/finalproject/suppliersystem/supplier/repository/ICountryCallingCodeRepository.java
+++ src/main/java/finalproject/suppliersystem/supplier/repository/ICountryCallingCodeRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ICallingCodeRepository extends JpaRepository<CountyCallingCode, Long>
+public interface ICountryCallingCodeRepository extends JpaRepository<CountyCallingCode, Long>
 {
 }
