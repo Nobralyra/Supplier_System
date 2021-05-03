0c2e9abbfc2f5b263d3cf5f578f0ea2b237dd07b
Nobralyra <signees96@gmail.com>
2021-05-03 16:07:43 +0200

    Tried to fixed problem with sortedset

--- src/main/java/finalproject/suppliersystem/supplier/domain/ProductCategory.java
+++ src/main/java/finalproject/suppliersystem/supplier/domain/ProductCategory.java
@@ -1,17 +1,16 @@
 package finalproject.suppliersystem.supplier.domain;
 
 import com.sun.istack.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.annotations.SortNatural;
 
 import javax.persistence.*;
 import javax.validation.constraints.Size;
-import java.util.SortedSet;
-import java.util.TreeSet;
+import java.util.*;
 
-@Data
+//@Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
@@ -32,6 +31,46 @@ public class ProductCategory implements Comparable<ProductCategory>
     @ManyToMany(mappedBy = "productCategorySet")
     @SortNatural
     private SortedSet<Supplier> supplierSet = new TreeSet<>();
+    //private List<Supplier> supplierSet = new ArrayList<>();
+
+//    private Set<Supplier> supplierSet = new HashSet<>();
+
+
+    /**
+     * Can't rely on a natural identifier for equality checks, so instead we use the entity identifier.
+     * The equality has to be consistent across all entity state transitions.
+     *
+     * To understand why to use equals and hashCode:
+     * https://docs.jboss.org/hibernate/orm/5.4/userguide/html_single/Hibernate_User_Guide.html#mapping-model-pojo-equalshashcode
+     * https://medium.com/@rajibrath20/the-best-way-to-map-a-onetomany-relationship-with-jpa-and-hibernate-dbbf6dba00d3
+     * https://vladmihalcea.com/how-to-implement-equals-and-hashcode-using-the-jpa-entity-identifier/
+     * https://vladmihalcea.com/the-best-way-to-map-a-onetomany-association-with-jpa-and-hibernate/
+     * https://stackoverflow.com/questions/9560522/hibernate-how-to-properly-organize-relation-a-one-to-many-with-annotations
+     *
+     * @param object - the object to be compared
+     * @return boolean
+     */
+    @Override
+    public boolean equals(Object object)
+    {
+        if (this == object)
+        {
+            return true;
+        }
+
+        if (!(object instanceof ProductCategory))
+        {
+            return false;
+        }
+
+        return productCategoryId != null && productCategoryId.equals(((ProductCategory)object).getProductCategoryId());
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return getClass().hashCode();
+    }
 
     /**
      * https://howtodoinjava.com/java/collections/java-comparable-interface/
--- src/main/java/finalproject/suppliersystem/supplier/domain/Supplier.java
+++ src/main/java/finalproject/suppliersystem/supplier/domain/Supplier.java
@@ -1,17 +1,14 @@
 package finalproject.suppliersystem.supplier.domain;
 
 import com.sun.istack.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.annotations.SortNatural;
 import org.hibernate.validator.constraints.UniqueElements;
 
 import javax.persistence.*;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
-import java.util.SortedSet;
-import java.util.TreeSet;
+import java.util.*;
 
 /**
  * https://projectlombok.org/features/Data
@@ -19,7 +16,9 @@ import java.util.TreeSet;
  * Validations:
  * https://docs.jboss.org/hibernate/stable/validator/reference/en-US/html_single/#validator-annotation-processor
  */
-@Data
+//@Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
@@ -55,9 +54,48 @@ public class Supplier implements Comparable<Supplier>
             inverseJoinColumns = @JoinColumn(name = "product_category_id")
     )
     @SortNatural
-    @UniqueElements
     private SortedSet<ProductCategory> productCategorySet = new TreeSet<>();
+//    private List<ProductCategory> productCategorySet = new ArrayList<>();
 
+    //@UniqueElements
+//    private Set<ProductCategory> productCategorySet = new HashSet<>();
+
+
+    /**
+     * Can't rely on a natural identifier for equality checks, so instead we use the entity identifier.
+     * The equality has to be consistent across all entity state transitions.
+     *
+     * To understand why to use equals and hashCode:
+     * https://docs.jboss.org/hibernate/orm/5.4/userguide/html_single/Hibernate_User_Guide.html#mapping-model-pojo-equalshashcode
+     * https://medium.com/@rajibrath20/the-best-way-to-map-a-onetomany-relationship-with-jpa-and-hibernate-dbbf6dba00d3
+     * https://vladmihalcea.com/how-to-implement-equals-and-hashcode-using-the-jpa-entity-identifier/
+     * https://vladmihalcea.com/the-best-way-to-map-a-onetomany-association-with-jpa-and-hibernate/
+     * https://stackoverflow.com/questions/9560522/hibernate-how-to-properly-organize-relation-a-one-to-many-with-annotations
+     *
+     * @param object - the object to be compared
+     * @return boolean
+     */
+    @Override
+    public boolean equals(Object object)
+    {
+        if (this == object)
+        {
+            return true;
+        }
+
+        if (!(object instanceof Supplier))
+        {
+            return false;
+        }
+
+        return supplierId != null && supplierId.equals(((Supplier)object).getSupplierId());
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return getClass().hashCode();
+    }
     /**
      * https://howtodoinjava.com/java/collections/java-comparable-interface/
      * @param o
