2a5c07dad0fdd780f5e4787350da1969e6530a29
Nobralyra <signees96@gmail.com>
2021-05-05 14:55:01 +0200

    Started adding exceptionhandling

--- src/main/java/finalproject/suppliersystem/core/ApiError.java
+++ src/main/java/finalproject/suppliersystem/core/ApiError.java
@@ -1,2 +1,51 @@
-package finalproject.suppliersystem.core;public class ApiError {
+package finalproject.suppliersystem.core;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Custom error message
+ */
+@Getter
+@Setter
+public class ApiError
+{
+    /**
+     * The HTTP status code
+     */
+    private HttpStatus status;
+
+    /**
+     * The error message associated with exception
+     */
+    private String message;
+
+    /**
+     * List of constructed error messages
+     */
+    private List<String> errors;
+
+    public ApiError()
+    {
+        super();
+    }
+
+
+    public ApiError(HttpStatus status, String message, List<String> errors) {
+        super();
+        this.status = status;
+        this.message = message;
+        this.errors = errors;
+    }
+
+    public ApiError(HttpStatus status, String message, String error) {
+        super();
+        this.status = status;
+        this.message = message;
+        errors = Arrays.asList(error);
+    }
 }
