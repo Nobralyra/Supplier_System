d7b9de3f3c19b5ea7279aebe7ada9d1bacc944c3
Päivi Eversbusch <paiv0018@stud.kea.dk>
2021-05-05 10:26:58 +0200

    unit tester i AddressService og Address

--- src/test/java/finalproject/suppliersystem/SuppliersystemApplicationTests.java
+++ src/test/java/finalproject/suppliersystem/SuppliersystemApplicationTests.java
@@ -3,6 +3,11 @@ package finalproject.suppliersystem;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * Tests, if the application starts
+ * This test is passed
+ */
+
 @SpringBootTest
 class SuppliersystemApplicationTests
 {
