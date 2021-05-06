a9bc80dba6e06c898e6ca86f13320503f1a57fee
Nobralyra <signees96@gmail.com>
2021-05-06 16:06:58 +0200

    Added Errorhandling

--- src/main/java/finalproject/suppliersystem/core/UnknownResourceException.java
+++ src/main/java/finalproject/suppliersystem/core/UnknownResourceException.java
@@ -1,2 +1,25 @@
-package finalproject.suppliersystem.core;public class UnknownResourceException {
+package finalproject.suppliersystem.core;
+
+/**
+ * Custom Exception to throw
+ * https://nixmash.com/java/custom-404-exception-handling-in-spring-mvc/
+ */
+public class UnknownResourceException extends Exception
+{
+    private static final long serialVersionUID = 3613978896775863909L;
+
+    private String message;
+
+    public UnknownResourceException() {
+        super();
+    }
+
+    public UnknownResourceException(String message) {
+        this.message = System.currentTimeMillis()
+                + ": " + message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
