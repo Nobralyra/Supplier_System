435e9ca7fd2a65a4a7662a1cbf31d33457fd4663
Nobralyra <signees96@gmail.com>
2021-05-12 11:35:47 +0200

    Added test for AlgrithmSupplierRiskLevelService

--- src/test/java/finalproject/suppliersystem/supplier/restapi/service/AlgorithmSupplierRiskLevelServiceTest.java
+++ src/test/java/finalproject/suppliersystem/supplier/restapi/service/AlgorithmSupplierRiskLevelServiceTest.java
@@ -1,4 +1,26 @@
+package finalproject.suppliersystem.supplier.restapi.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
-class AlgorithmSupplierRiskLevelServiceTest {
-  
+
+class AlgorithmSupplierRiskLevelServiceTest
+{
+    private AlgorithmSupplierRiskLevelService algorithmSupplierRiskLevelService;
+
+    @BeforeEach
+    void setUp()
+    {
+        algorithmSupplierRiskLevelService = new AlgorithmSupplierRiskLevelService();
+    }
+
+    /**
+     * This tests, that context is creating an algorithmSupplierRiskLevelService.
+     */
+    @Test
+    public void contextLoads()
+    {
+        assertNotNull(algorithmSupplierRiskLevelService);
+    }
 }
