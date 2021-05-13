ff070dc099b2557a0746a12845946dc33477b1ac
Nobralyra <signees96@gmail.com>
2021-05-13 11:31:28 +0200

    Maded RestService an interface

--- src/main/java/finalproject/suppliersystem/supplier/restapi/service/ICalculatorSupplierRiskLevelRestService.java
+++ src/main/java/finalproject/suppliersystem/supplier/restapi/service/ICalculatorSupplierRiskLevelRestService.java
@@ -1,2 +1,9 @@
-package finalproject.suppliersystem.supplier.restapi.service;public interface ICalculatorSupplierRiskLevelRestService {
+package finalproject.suppliersystem.supplier.restapi.service;
+
+import finalproject.suppliersystem.core.enums.CorporateSocialResponsibility;
+import reactor.core.publisher.Mono;
+
+public interface ICalculatorSupplierRiskLevelRestService
+{
+    Mono<String> calculateSupplierRiskLevel(CorporateSocialResponsibility corporateSocialResponsibility, int issuesConcerningCooperation, int availabilityIssues);
 }
