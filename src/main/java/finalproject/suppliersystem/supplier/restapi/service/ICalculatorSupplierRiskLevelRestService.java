75879b577812b24a535a0341b5d49e7313e0cbe5
Nobralyra <signees96@gmail.com>
2021-05-14 11:18:14 +0200

    Changed returntype to ENUM

--- src/main/java/finalproject/suppliersystem/supplier/restapi/service/ICalculatorSupplierRiskLevelRestService.java
+++ src/main/java/finalproject/suppliersystem/supplier/restapi/service/ICalculatorSupplierRiskLevelRestService.java
@@ -1,9 +1,8 @@
 package finalproject.suppliersystem.supplier.restapi.service;
 
-import finalproject.suppliersystem.core.enums.CorporateSocialResponsibility;
-import reactor.core.publisher.Mono;
+import finalproject.suppliersystem.core.enums.CategoryLevel;
 
 public interface ICalculatorSupplierRiskLevelRestService
 {
-    Mono<String> calculateSupplierRiskLevel(CorporateSocialResponsibility corporateSocialResponsibility, int issuesConcerningCooperation, int availabilityIssues);
+    Enum<CategoryLevel> calculateSupplierRiskLevel(CategoryLevel corporateSocialResponsibility, int issuesConcerningCooperation, int availabilityIssues);
 }
