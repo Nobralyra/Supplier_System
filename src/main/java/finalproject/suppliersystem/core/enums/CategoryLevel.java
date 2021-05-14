20acd0f1e7129a9b277f06f70124c27a4d276a16
Nobralyra <signees96@gmail.com>
2021-05-14 11:17:42 +0200

    Changed order and name of ENUM

--- src/main/java/finalproject/suppliersystem/core/enums/CategoryLevel.java
+++ src/main/java/finalproject/suppliersystem/core/enums/CategoryLevel.java
@@ -1,12 +1,13 @@
 package finalproject.suppliersystem.core.enums;
 
+
 /**
  * These values are used in Criticality
  */
-public enum CorporateSocialResponsibility {
 
-    HIGH(), //id 0
+public enum CategoryLevel
+{
+    LOW(), //id 0
     MEDIUM(), // id 1
-    LOW() // id 2;
-
+    HIGH() // id 2;
 }
