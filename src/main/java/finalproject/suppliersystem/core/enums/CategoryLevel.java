7c756ff1ff7f655201fca411033bc44834ddf962
Nobralyra <signees96@gmail.com>
2021-05-14 16:11:02 +0200

    Deleted semicolon

--- src/main/java/finalproject/suppliersystem/core/enums/CategoryLevel.java
+++ src/main/java/finalproject/suppliersystem/core/enums/CategoryLevel.java
@@ -3,10 +3,9 @@ package finalproject.suppliersystem.core.enums;
 /**
  * These values are used in Criticality
  */
-
 public enum CategoryLevel
 {
     LOW(), //id 0
     MEDIUM(), // id 1
-    HIGH() // id 2;
+    HIGH() // id 2
 }
