7a51e6f9d671e9d15a46b11807e264b397b6db92
Nobralyra <signees96@gmail.com>
2021-05-21 08:54:33 +0200

    Added Controller to Supplier Criticality

--- src/main/java/finalproject/suppliersystem/supplier/controller/OverviewCriticalityController.java
+++ src/main/java/finalproject/suppliersystem/supplier/controller/OverviewCriticalityController.java
@@ -1,8 +1,17 @@
 package finalproject.suppliersystem.supplier.controller;
 
+import finalproject.suppliersystem.supplier.criticalityview.SupplierProductCategoryCriticalityView;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class OverwiewCriticalityController
+public class OverviewCriticalityController
 {
+    @GetMapping("/supplier/criticality")
+    public String showSupplierCriticality(SupplierProductCategoryCriticalityView supplierProductCategoryCriticalityView, Model model)
+    {
+        model.addAttribute("supplierProductCategoryCriticalityView", supplierProductCategoryCriticalityView);
+        return "/supplier/criticality";
+    }
 }
