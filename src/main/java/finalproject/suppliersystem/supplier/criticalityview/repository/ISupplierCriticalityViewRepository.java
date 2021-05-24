bde56f15561b4f0214373b9c41b4f07ac8ae83c9
Nobralyra <signees96@gmail.com>
2021-05-24 15:55:35 +0200

    added annotation

--- src/main/java/finalproject/suppliersystem/supplier/criticalityview/repository/ISupplierCriticalityViewRepository.java
+++ src/main/java/finalproject/suppliersystem/supplier/criticalityview/repository/ISupplierCriticalityViewRepository.java
@@ -2,7 +2,9 @@ package finalproject.suppliersystem.supplier.criticalityview.repository;
 
 import finalproject.suppliersystem.supplier.criticalityview.view.SupplierCriticalityView;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ISupplierCriticalityViewRepository extends JpaRepository<SupplierCriticalityView, Long>
 {
 }
