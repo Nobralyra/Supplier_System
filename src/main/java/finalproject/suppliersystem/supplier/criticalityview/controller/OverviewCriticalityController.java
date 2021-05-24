e97927f579116943c4be990b133259262dc41e1b
Nobralyra <signees96@gmail.com>
2021-05-24 15:55:52 +0200

    Controller now only show the page

--- src/main/java/finalproject/suppliersystem/supplier/criticalityview/controller/OverviewCriticalityController.java
+++ src/main/java/finalproject/suppliersystem/supplier/criticalityview/controller/OverviewCriticalityController.java
@@ -1,10 +1,15 @@
 package finalproject.suppliersystem.supplier.criticalityview.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import finalproject.suppliersystem.supplier.criticalityview.service.ISupplierCriticalityViewService;
+import finalproject.suppliersystem.supplier.criticalityview.view.SupplierCriticalityView;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 public class OverviewCriticalityController
@@ -20,7 +25,6 @@ public class OverviewCriticalityController
     @GetMapping("/supplier/criticality")
     public String showSupplierCriticality(Model model)
     {
-        model.addAttribute("supplierCriticalityView", iSupplierCriticalityViewService.findAll());
         return "/supplier/criticality";
     }
 }
