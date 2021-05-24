bcc222fbba6c1079a39bad05ed3c21eb6c31a5b6
Nobralyra <signees96@gmail.com>
2021-05-24 15:56:06 +0200

    Restcontroller to get the data to the table

--- src/main/java/finalproject/suppliersystem/supplier/criticalityview/restcontroller/OverviewCriticalityRestController.java
+++ src/main/java/finalproject/suppliersystem/supplier/criticalityview/restcontroller/OverviewCriticalityRestController.java
@@ -1,2 +1,60 @@
-package finalproject.suppliersystem.supplier.criticalityview;public class OverviewCriticalityRestController {
+package finalproject.suppliersystem.supplier.criticalityview.restcontroller;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import finalproject.suppliersystem.supplier.criticalityview.service.ISupplierCriticalityViewService;
+import finalproject.suppliersystem.supplier.criticalityview.view.SupplierCriticalityView;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @RestController is a specialized version of the Controller. It's a REST style excelcontroller
+ * Handles methods that shall return JSON/XML response directly to the client
+ * Has @Controller and @ResponseBody
+ * https://howtodoinjava.com/spring-boot2/rest/controller-restcontroller/
+ */
+ @RestController
+public class OverviewCriticalityRestController
+{
+    private final ISupplierCriticalityViewService iSupplierCriticalityViewService;
+
+    @Autowired
+    public OverviewCriticalityRestController(ISupplierCriticalityViewService iSupplierCriticalityViewService)
+    {
+        this.iSupplierCriticalityViewService = iSupplierCriticalityViewService;
+    }
+
+
+    /**
+     * Gets executed when a request that matches the url specified in the @GetMapping.
+     * https://www.baeldung.com/jackson-vs-gson
+     * @return String
+     */
+    @GetMapping(value = "/supplier_criticality_table_api", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String getSupplierCriticalityData()
+    {
+        List<SupplierCriticalityView> test =  iSupplierCriticalityViewService.findAll();
+        //create ObjectMapper instance
+        ObjectMapper objectMapper = new ObjectMapper();
+        String newJsonData = "Data";
+        try
+        {
+            newJsonData = objectMapper.writeValueAsString(test);
+        }
+        catch (JsonProcessingException e)
+        {
+            e.printStackTrace();
+            System.err.println(e);
+        }
+
+        System.out.println(newJsonData);
+
+        return newJsonData;
+    }
 }
