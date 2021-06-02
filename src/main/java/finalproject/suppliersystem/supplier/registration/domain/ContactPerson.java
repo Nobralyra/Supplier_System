7eede830928b3a0fd1fd20d69d78cda373dbe5a0
Nobralyra <signees96@gmail.com>
2021-06-02 10:59:29 +0200

    Added hashcode and eqeuals method

--- src/main/java/finalproject/suppliersystem/supplier/registration/domain/ContactPerson.java
+++ src/main/java/finalproject/suppliersystem/supplier/registration/domain/ContactPerson.java
@@ -44,4 +44,40 @@ public class ContactPerson extends Audition
      */
     @ManyToOne (fetch = FetchType.LAZY)
     private ContactInformation contactInformation;
+
+    /**
+     * Can't rely on a natural identifier for equality checks, so instead we use the entity identifier.
+     * The equality has to be consistent across all entity state transitions.
+     *
+     * To understand why to use equals and hashCode:
+     * https://docs.jboss.org/hibernate/orm/5.4/userguide/html_single/Hibernate_User_Guide.html#mapping-model-pojo-equalshashcode
+     * https://medium.com/@rajibrath20/the-best-way-to-map-a-onetomany-relationship-with-jpa-and-hibernate-dbbf6dba00d3
+     * https://vladmihalcea.com/how-to-implement-equals-and-hashcode-using-the-jpa-entity-identifier/
+     * https://vladmihalcea.com/the-best-way-to-map-a-onetomany-association-with-jpa-and-hibernate/
+     * https://stackoverflow.com/questions/9560522/hibernate-how-to-properly-organize-relation-a-one-to-many-with-annotations
+     *
+     * @param object - the object to be compared
+     * @return boolean
+     */
+    @Override
+    public boolean equals(Object object)
+    {
+        if (this == object)
+        {
+            return true;
+        }
+
+        if (!(object instanceof ContactPerson))
+        {
+            return false;
+        }
+
+        return contactPersonId != null && contactPersonId.equals(((ContactPerson)object).getContactPersonId());
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return getClass().hashCode();
+    }
 }
